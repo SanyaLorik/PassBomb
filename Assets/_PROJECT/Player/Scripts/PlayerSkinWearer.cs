601c917ce285aec202b8592fc228a58b1cd6cda2
ance1ad <artem_tvinkov@mail.ru>
2026-04-26 18:47:14 +0300

    add skin base logic

---
+++ Assets/_PROJECT/Player/Scripts/PlayerSkinWearer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Architecture_M;
+using MediaKit_M.SkinChanger;
+using UnityEngine;
+using Zenject;
+
+public class PlayerSkinWearer : MonoBehaviour {
+    [Inject] IGameSave gameSave;
+    [Inject] private ISkinSaveLoader _skinSaveLoader;
+    
+    private SkinSave skinSave => _skinSaveLoader.Load();
+
+    private void OnEnable() {
+        skinSave.OnWearUpdated += WearUpdate;
+    }
+
+    private void Start() {
+        WearUpdate(skinSave.WearSkins);
+        
+    }
+
+    private void WearUpdate(IReadOnlyList<KeyValuePair<int, SkinData>> allSkins) {
+        // Tab и Скин, мне нужно снимать текущий скин и надевать новый,
+        // можно пробежаться по всем скинам из таба
+        foreach (KeyValuePair<int, SkinData> skin in allSkins) {
+            
+        }
+    }
+}
---
+++ Assets/_PROJECT/Player/Scripts/SkinWearer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct ClothInfo {
+    public string ClothId;
+    public GameObject ClothObject;
+    public GameObject BodyPartObject;
+    // Если не нужна боди парт снимаем все их с TabId
+}
+
+
+[Serializable]
+public struct BodyPart {
+    public int TabId;
+    public GameObject[] BodyPartObjects;
+}
+
+
+[Serializable]
+public struct Clothes {
+    public int TabId;
+    public ClothInfo[] ClothesInfo;
+}
+
+
+public class SkinWearer : MonoBehaviour {
+    [SerializeField] private Clothes[] Clothes;
+    [SerializeField] private BodyPart[] BodyParts;
+
+}
